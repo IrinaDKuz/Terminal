6485f41973f5c4ad3f6101c7353001c51939cfb7
Irina <kyznezova1992@yandex.ru>
2023-04-20 15:26:10 +0200

    some syntax changes

--- script.sh
+++ script.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-
+# to run this script use "bash script.sh" on Terminal
 FOLDER="folder_4"
-
-rm -rf $FOLDER
 echo "remove ${FOLDER} with content by force"
-mkdir $FOLDER
+rm -rf $FOLDER
 echo "create ${FOLDER}"
-cd $FOLDER
+mkdir $FOLDER
 echo "go to ${FOLDER}"
-
-mkdir fol_1 fol_2 fol_3
+cd $FOLDER
 echo "create 3 new folders"
-cd fol_1
+mkdir fol_1 fol_2 fol_3
 echo "go to fol_1"
-cd ..
+cd fol_1
 echo "go back to ${FOLDER}"
-touch file1.json file2.json note1.txt note2.txt note3.txt
+cd ..
 echo "create 5 new files"
+touch file1.json file2.json note1.txt note2.txt note3.txt
 echo "${FOLDER} contains:"
 ls -la
-mv file1.json file2.json fol_2/
 echo "move 2 files to fol_2"
-
-
-
+mv file1.json file2.json fol_2/
 exit 0
 
